344c60fa63bf7250ccaee2069a62364198e68a0f
Erich Boschert <erich.boschert10@gmail.com>
2025-04-30 12:06:17 -0400

    add stacktrace + dont test for now

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -3,11 +3,7 @@
  */
 package org.example;
 
-import java.nio.file.Paths;
-
 import org.junit.jupiter.api.Test;
-
-import io.github.freshsupasulley.JScribe;
 
 class AppTest {
 	
@@ -18,21 +14,21 @@ class AppTest {
 	@Test
 	void testModel()
 	{
-		JScribe scribe = new JScribe(Paths.get("src/test/resources/ggml-tiny.en.bin"));
-		scribe.start("", 1000, 500, true);
-		
-		// Translate for a while
-		long start = System.currentTimeMillis(), lastAudio = start;
-		
-		while(System.currentTimeMillis() - start < 30000 && !scribe.noAudio())
-		{
-			if(System.currentTimeMillis() - lastAudio > 200)
-			{
-				lastAudio = System.currentTimeMillis();
-				System.out.println("Audio level: " + scribe.getAudioLevel());
-			}
-			
-			for(String buffer = null; !(buffer = scribe.getBuffer()).equals(""); System.out.println(buffer));
-		}
+//		JScribe scribe = new JScribe(Paths.get("src/test/resources/ggml-tiny.en.bin"));
+//		scribe.start("", 1000, 500, true);
+//		
+//		// Translate for a while
+//		long start = System.currentTimeMillis(), lastAudio = start;
+//		
+//		while(System.currentTimeMillis() - start < 30000 && !scribe.noAudio())
+//		{
+//			if(System.currentTimeMillis() - lastAudio > 200)
+//			{
+//				lastAudio = System.currentTimeMillis();
+//				System.out.println("Audio level: " + scribe.getAudioLevel());
+//			}
+//			
+//			for(String buffer = null; !(buffer = scribe.getBuffer()).equals(""); System.out.println(buffer));
+//		}
 	}
 }
