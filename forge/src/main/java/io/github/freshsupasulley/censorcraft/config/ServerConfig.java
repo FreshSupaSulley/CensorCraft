697ab4c29bb98fe2bbc74bc2f3e7fdfc7946c1db
Erich Boschert <erich.boschert10@gmail.com>
2025-09-20 17:51:18 -0400

    whoops tiny.en snuck in

--- forge/src/main/java/io/github/freshsupasulley/censorcraft/config/ServerConfig.java
+++ forge/src/main/java/io/github/freshsupasulley/censorcraft/config/ServerConfig.java
@@ -159,7 +159,7 @@ public class ServerConfig extends ConfigFile {
 	void register(ConfigSpec spec)
 	{
 		defineList("global_taboos", List.of("boom"), "List of forbidden words and phrases (case-insensitive)", "All enabled punishments will fire when they are spoken");
-		define("preferred_model", "base.en", "Name of the transcription model players need to use (determines the language and accuracy)", "Better models have larger file sizes. Clients have tiny.en built-in. See https://github.com/ggml-org/whisper.cpp/blob/master/models/README.md#available-models for available models");
+		define("preferred_model", "base.en", "Name of the transcription model players need to use (determines the language and accuracy)", "Better models have larger file sizes. See https://github.com/ggml-org/whisper.cpp/blob/master/models/README.md#available-models for available models");
 		defineInRange("context_length", 3D, 0D, Double.MAX_VALUE, "Maximum amount of time (in seconds) an individual audio recording is. The higher the value, the more intensive on players PCs", "Enter a number to at least 1 decimal place!");
 		defineInRange("punishment_cooldown", 0D, 0D, Double.MAX_VALUE, "Delay (in seconds) before a player can be punished again", "Enter a number to at least 1 decimal place!");
 		define("chat_taboos", true, "When someone is punished, send what the player said to chat");
